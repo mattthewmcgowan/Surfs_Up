bd6175e0cd61047251005695c0f53504d6fca200
Honest <TMAM9@unigroupinc.com>
2022-07-30 11:46:31 -0500

    Mod Code

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-        return 'Hello World'
+        return 'Sup'
         
 if __name__ == "__main__":
     app.run(debug=True)
